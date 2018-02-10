cad52c1dcb7fcf88d80e148ddbf660382b38488b
SwiftKobe <folledoswag@gmail.com>
2018-02-10 03:29:24 -0500

    Initial Commit

---
+++ ImranAssignment1Part1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ImranAssignment1Part1
+//
+//  Created by Samuel Folledo on 2/10/18.
+//  Copyright © 2018 Samuel Folledo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
